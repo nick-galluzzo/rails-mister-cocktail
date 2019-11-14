d5e102118b84913e81532ce93d609f460d4c71b4
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-14 13:47:26 +0000

    dose ingredient cocktail models and associations completed

--- db/migrate/20191114122737_create_doses.rb
+++ db/migrate/20191114122737_create_doses.rb
@@ -2,8 +2,8 @@ class CreateDoses < ActiveRecord::Migration[5.2]
   def change
     create_table :doses do |t|
       t.text :description, null: false
-      t.references :cocktail, foreign_key: true
-      t.references :ingredient, foreign_key: true
+      t.references :cocktail, foreign_key: true, unique: true
+      t.references :ingredient, foreign_key: true, unique: true
 
       t.timestamps
     end
