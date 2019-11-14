27543ef3aaca6ac19f44e16249a3c8a0f1c659a5
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-14 12:25:27 +0000

    created ingredient and cocktail model

---
+++ db/migrate/20191114121541_create_cocktails.rb
@@ -0,0 +1,9 @@
+class CreateCocktails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cocktails do |t|
+      t.string :name, null: false, unique: true
+
+      t.timestamps
+    end
+  end
+end
