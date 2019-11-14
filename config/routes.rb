4f7ed274b5368269ba5190c45e3e22824cb59f20
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-14 15:19:13 +0000

    route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'doses/create'
-  get 'doses/new'
-  get 'doses/delete'
   resources :cocktails, only: [:index, :show, :create, :new] do
     resources :doses, only: [:create, :new, :delete]
   end
