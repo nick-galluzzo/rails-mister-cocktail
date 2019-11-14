8832339fb2241a586143f06b262b39304e6fed99
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-14 15:16:29 +0000

    added route associations to cocktails of doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :create, :new]
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:index, :create, :new, :delete]
+  end
 end
