f5c82c4d95fd3c2c6be8f73b168554b52276fce6
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-14 14:53:52 +0000

    added 10 ingredients to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url_serialized = open(url).read
+drinks = JSON.parse(url_serialized)
+
+DRINK_LIST = drinks["drinks"].map {|drink| drink['strIngredient1'] }
+
+
+#  add 10 drinks
+10.times do
+  Ingredient.create(name: DRINK_LIST[rand(0..DRINK_LIST.length - 1)])
+end
