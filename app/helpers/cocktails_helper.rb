cecaf326a285b7236baa3a3147753585ac9761bc
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-15 11:53:56 +0000

    add dotenv- protect secret data in .env'

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,14 +1,2 @@
 module CocktailsHelper
-  IMAGES = %w[
-    old-fashioned
-    bloody-mary
-    margarita
-    mojito
-    moscow-mule
-    negroni
-  ]
-
-  def picturize(index)
-    "#{IMAGES[index]}.jpeg"
-  end
 end
