43f96bca3f650535b8f4ed3b96e6e473d9b40e97
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-15 11:34:07 +0000

    changed root directory to cocktails#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
     resources :doses, only: [:create, :new]
   end
