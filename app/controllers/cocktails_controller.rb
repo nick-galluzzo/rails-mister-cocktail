3704cfcb56589a335e1e83e59c1ccf73fa4b6fee
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-15 12:24:56 +0000

    functional image uploading

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -33,6 +33,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :dose, :ingredient, :ingredient_id)
+    params.require(:cocktail).permit(:name, :dose, :ingredient, :ingredient_id, :photo)
   end
 end
