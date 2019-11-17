d9cbe45fc3a32e45451d34437d266733057a6d49
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-17 16:59:55 +0000

    added rating inclusion 1..5

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :cocktail
 
   validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (1..5) }
 end
