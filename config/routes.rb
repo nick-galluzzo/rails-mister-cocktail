1468e49923091ba71347144950e54c1d71252bcd
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-17 12:30:25 +0000

    routing for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
     resources :doses, only: [:create, :new]
+    resources :reviews, only: [:create, :new]
   end
 
+  resources :reviews, only: [:destroy]
   resources :doses, only: [:destroy]
 end
